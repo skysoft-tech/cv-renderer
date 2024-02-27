905bec1c0f185d3459884412c82b94a6ade2a98c
13Vitalik13 <vytalikt13@gmail.com>
2024-02-27 15:41:31 +0200

    Fix merge conflict

--- SkySoft.CvRenderer.Cli/AppOptions.cs
+++ SkySoft.CvRenderer.Cli/AppOptions.cs
@@ -25,5 +25,11 @@ namespace SkySoft.CvRenderer.Cli
         [CliOptionName("--workColumnWidth", "-w")]
         [CliOptionDescription("Allows to change width of the first column in Work Experience section")]
         public int WorkColumnWidth { get; set; } = 60;
+
+        [DefaultValue(true)]
+        [OptionKeyName($"Rendering:{nameof(HideLogo)}")]
+        [CliOptionName("--hideLogo", "-h")]
+        [CliOptionDescription("Allows you to disable the logo in the header")]
+        public bool HideLogo { get; set; } = true;
     }
 }
--- SkySoft.CvRenderer.Cli/Program.cs
+++ SkySoft.CvRenderer.Cli/Program.cs
@@ -16,36 +16,11 @@ namespace SkySoft.CvRenderer.Cli
                 .Build();
 
             var options = config.Get<AppOptions>();
-            
+
             var logger = Logger.SetupLogger();
             var executor = new Executor(logger);
 
-            var companyColumnWidthOption = new Option<int>(
-                aliases: new string[] { "--columnWidth", "-w" },
-                description: "Allows to change width of the first column in Work Experience section", 
-                getDefaultValue: () => 60);
-
-            var companyHideLogo = new Option<bool>(
-                 aliases: new string[] { "--hideLogo", "-h" },
-                 description: "Allows you to hide the logo in the header",
-                 getDefaultValue: () => false);
-
-            var rootCommand = new RootCommand("Allow to render PDF based on CV data");
-            rootCommand.AddOption(inputOption);
-            rootCommand.AddOption(outputOption);
-            rootCommand.AddOption(companyColumnWidthOption);
-            rootCommand.AddOption(companyHideLogo);
-
-            rootCommand.SetHandler(async (input, output, width, hideLogo) =>
-            {
-                var logger = Logger.SetupLogger();
-                var executor = new Executor(logger);
-
-                await executor.Run(input, output, width, hideLogo);
-            }, inputOption, outputOption, companyColumnWidthOption, companyHideLogo);
-
-            return await rootCommand.InvokeAsync(args);
-            await executor.Run(options.InputFile, options.OutputFile, options.Rendering.WorkColumnWidth);
+            await executor.Run(options.InputFile, options.OutputFile, options.Rendering.WorkColumnWidth, options.Rendering.HideLogo);
         }
     }
 }
